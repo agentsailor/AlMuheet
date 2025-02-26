182f846331cbcdd6b6399af888c404f9b24878f2
sailoragent <designaate@icloud.com>
2025-02-26 19:24:12 +0200

    Update account.tsx

--- app/(tabs)/account.tsx
+++ app/(tabs)/account.tsx
@@ -36,8 +36,8 @@ export default function Account() {
 
   const [note, onChangeNote] = React.useState('');
   const [pName, onChangePname] = React.useState('');
-  // const [path, setPath] = React.useState('http://127.0.0.1:8000');
-  const [path, setPath] = React.useState('https://www.almuheetco.com');
+  const [path, setPath] = React.useState('http://127.0.0.1:8000');
+  // const [path, setPath] = React.useState('https://www.almuheetco.com');
 
   useEffect(() => {
     if (user == undefined) {
@@ -46,7 +46,12 @@ export default function Account() {
           setUser(JSON.parse(data[0][1]))
           onChangeName(JSON.parse(data[0][1]).name)
           onChangeMail(JSON.parse(data[0][1]).email)
-          onChangeNumber(JSON.parse(data[0][1]).phone)
+          onChangePhone(JSON.parse(data[0][1]).phone)
+
+          onChangeStore(JSON.parse(data[0][1]).store.name)
+          onChangeNumber(JSON.parse(data[0][1]).store.number)
+          onChangeLocation(JSON.parse(data[0][1]).store.location)
+
         }else {
           setUser(null)
         }
@@ -56,7 +61,7 @@ export default function Account() {
 
 
   useEffect(() => {
-      axios.post(path+'/api/render',{for:'helpers',})
+      axios.post(path+'/api/render',{for:'helpers',user:user ? user : undefined})
       .then(res => {
         setSettings(res.data.settings)
         setLang(res.data.lang == 'en' ? 0:1)
@@ -77,20 +82,27 @@ export default function Account() {
     console.log(user);
 
     return (<View style={{padding:20}}>
+
+
       <TextInput require value={name} name="name" onChangeText={onChangeName} type="text" style={{marginTop:20,borderRadius:20,overflow:'hidden',backgroundColor:'#fff', height: 60, color:'#000', textAlign:lang ? 'right':'left', width:'100%', border:'1px solid #ccc', padding: 10,  textAlignLast:lang ? 'right':'left'}} placeholder={render('content','fname')} />
       <TextInput value={mail} name="number" onChangeText={onChangeMail} type="phone-pad" style={{marginTop:20,borderRadius:20,overflow:'hidden',backgroundColor:'#fff', height: 60, color:'#000', textAlign:lang ? 'right':'left', width:'100%', border:'1px solid #ccc', padding: 10,  textAlignLast:lang ? 'right':'left'}} placeholder={render('content','email')}/>
-      <TextInput require value={number} name="number" onChangeText={onChangeNumber} type="phone-pad" style={{marginTop:20,borderRadius:20,overflow:'hidden',backgroundColor:'#fff', height: 60, color:'#000', textAlign:lang ? 'right':'left', width:'100%', border:'1px solid #ccc', padding: 10,  textAlignLast:lang ? 'right':'left'}} placeholder={render('content','phone')} keyboardType="numeric"/>
+      <TextInput require value={phone} name="phone" onChangeText={onChangePhone} type="phone-pad" style={{marginTop:20,borderRadius:20,overflow:'hidden',backgroundColor:'#fff', height: 60, color:'#000', textAlign:lang ? 'right':'left', width:'100%', border:'1px solid #ccc', padding: 10,  textAlignLast:lang ? 'right':'left'}} placeholder={render('content','phone')} keyboardType="numeric"/>
 
 
-
+      <View style={{marginTop:20,borderWidth:1,border:'1px solid #17233c',borderRadius:20,overflow:'hidden',}}>
+        <Text style={{textAlign:'center',color:'#fff',padding:10,backgroundColor:'#17233c',fontWeight:600}} type="subtitle">{render('content','storeData')}</Text>
+        <TextInput require value={store} name="store" onChangeText={onChangeStore} type="text" style={{ height: 40, color:'#fff', textAlign:lang ? 'right':'left', width:'100%', borderBottomWidth:1, padding: 10,color:'#000'}} placeholder={render('content','name')} />
+        <TextInput value={number} name="number" onChangeText={onChangeNumber} type="phone-pad" style={{ height: 40, color:'#fff', textAlign:lang ? 'right':'left', width:'100%', borderBottomWidth:1, padding: 10,color:'#000',}} placeholder={render('content','phone')} keyboardType="numeric"/>
+        <TextInput require value={location} name="location" onChangeText={onChangeLocation} type="text" multiline={true} rows={5} style={{ height: 40, color:'#fff', textAlign:lang ? 'right':'left', width:'100%', borderBottomWidth:1, padding: 10,height:70,color:'#000',borderBottomWidth:0}} placeholder={render('content','address')} />
+      </View>
 
       <View style={{position:'fixed',bottom:100,right:0,left:0,margin:20,borderRadius:20,overflow:'hidden'}}>
         <TouchableOpacity style={Boolean(false) ? {alignItems:'center',padding:20,backgroundColor:'#ccc',color:'#000',fontWeight:700}:{alignItems:'center',padding:20,backgroundColor:'#fcbe0e',color:'#000',fontWeight:700}} variant='contained' endIcon={<IconSymbol size={30} name="save.fill" color="#000"  />}
-        onPress={()=> axios.post(path+'/api/render',{ for:'account',trigger:'update' ,name:name,phone:number,mail:mail,store:store,location:location, })
+        onPress={()=> axios.post(path+'/api/render',{ for:'account',trigger:'update' ,user:user,id:user.id,name:name,phone:phone,number:number,email:mail,store:store,location:location, })
         .then(async(res)=> {
-          const data = JSON.stringify(res.data.public);
-          // 
-          // await AsyncStorage.multiSet([['publicData', data]])
+          const data = JSON.stringify(res.data);
+
+          await AsyncStorage.multiSet([['publicData', data]])
 
         })}>
           <Text>{render('content','save')}</Text>
